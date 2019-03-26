caac298b758b3f05d2224cb71fd9f252a7e5448d
Arturo Rojas <arturorojas@hotmail.com>
2019-03-25 22:48:06 -0700

    v3

--- sw.js
+++ sw.js
@@ -2,20 +2,16 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/1891c25ec96ff3bbe21f.js",
-    "revision": "a0dffdf579e98626ae6eb55e2d74b443"
-  },
-  {
     "url": "/_nuxt/274341b41a0bf67f0016.js",
     "revision": "53133999fcee6c66df999502a2a86463"
   },
   {
-    "url": "/_nuxt/3b1cfa3652ece54d1cf7.js",
-    "revision": "a998903763c2cc933dd7f826ee0a6422"
+    "url": "/_nuxt/36b22c8e0219bc176268.js",
+    "revision": "885171519ce59963acbba2e61bf3551a"
   },
   {
-    "url": "/_nuxt/94b008b47e19c35819b9.js",
-    "revision": "3bf72be4130143d0656a35c1d523e456"
+    "url": "/_nuxt/3b1cfa3652ece54d1cf7.js",
+    "revision": "a998903763c2cc933dd7f826ee0a6422"
   },
   {
     "url": "/_nuxt/cb391c6963c4e36c3535.js",
@@ -26,8 +22,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "d048e34afe5c3ce30eebdda3f0565adb"
   },
   {
-    "url": "/_nuxt/fb78271825bdc27c1338.js",
-    "revision": "b1c7409b8454659d905352cebe5e0068"
+    "url": "/_nuxt/db6a54d3fae1218e9af4.js",
+    "revision": "182942788169dbdae6034a77fddc289c"
+  },
+  {
+    "url": "/_nuxt/e1306848a7d32bf39437.js",
+    "revision": "c1cecb85b0748c1cdf8f6bc6f396a0ca"
   }
 ], {
   "cacheId": "innart-webpage",
